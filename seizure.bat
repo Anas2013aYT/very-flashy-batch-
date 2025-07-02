62311cc0f8bc06891b10e2fd66ed9e92b4e1ffe0
Anas2013aYT <anasbinnoor4@gmail.com>
2025-07-03 00:02:53 +0400

    Update seizure.bat

--- seizure.bat
+++ seizure.bat
@@ -1,1 +1,15 @@
 echo off
+:red
+color 33
+echo COMPUTER SEIZURE!!!
+echo COMPUTER SEIZURE!!!!!
+goto green
+:green
+color 21
+echo COMPUTER SEIZURE!!!!!!!
+echo COMPUTER SEIZURE!!!!!!!!!
+goto blue
+:blue
+echo COMPUTER SEIZURE!!!!!!!
+echo COMPUTER SEIZURE!!!
+goto red
